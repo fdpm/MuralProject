23929411a30870deb107013b27c4e62497842cf6
Fabian Portilla <fdportillam1@gmail.com>
2021-06-10 02:16:08 -0500

    Referenced some fragments

--- theMural/app/src/main/java/com/example/themural/HomeActivity.java
+++ theMural/app/src/main/java/com/example/themural/HomeActivity.java
@@ -1,14 +1,62 @@
 package com.example.themural;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+//Actividad principal
 public class HomeActivity extends AppCompatActivity {
+
+    //Donde hosteamos las actividades a llamar
+    private NewItemFragment newItemFragment;
+    private ListItemFragment listItemFragment;
+    private BottomNavigationView navigator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        navigator = findViewById(R.id.navigator);
+
+        newItemFragment = NewItemFragment.newInstance();
+        listItemFragment = ListItemFragment.newInstance();
+
+        showFragment(newItemFragment);
+        navigator.setOnNavigationItemSelectedListener(
+                (menuItem)->{
+                    switch (menuItem.getItemId()){
+                        case R.id.listItem:
+                            //Aqui se especifica el fragmento a mostrar
+                            showFragment(newItemFragment);
+                            break;
+                        case R.id.addItem:
+                            showFragment(listItemFragment);
+                            break;
+                        case R.id.chatItem:
+                            //showFragment();
+                            break;
+                        case R.id.profileItem:
+                            //showFragment();
+                            break;
+                    }
+                    return true;
+                }
+        );
+    }
+
+    public void showFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        //Aqui se carga un fragmento por vez
+        transaction.replace(R.id.fragmentContainer, fragment);
+        transaction.commit();
+
     }
 }
--- theMural/app/src/main/java/com/example/themural/NewItemFragment.java
+++ theMural/app/src/main/java/com/example/themural/NewItemFragment.java
@@ -7,13 +7,16 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link NewItemFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class NewItemFragment extends Fragment {
+public class NewItemFragment extends Fragment implements View.OnClickListener{
 
 
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +27,8 @@ public class NewItemFragment extends Fragment {
     //private String mParam1;
     //private String mParam2;
 
-    //
+    private EditText editTextN;
+    private Button button2;
 
     public NewItemFragment() {
         // Required empty public constructor
@@ -49,10 +53,29 @@ public class NewItemFragment extends Fragment {
 
 
     //El fragment se vuelve visible
+    //Referenciar todos los elementos aqui para que se puedan ver
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_new_item, container, false);
+        View root = inflater.inflate(R.layout.fragment_new_item, container, false);
+
+        //Elementos referenciados del xml respectivo
+        editTextN = root.findViewById(R.id.editTextN);
+        button2 = root.findViewById(R.id.button2);
+
+        //para programar el boton
+        button2.setOnClickListener(this);
+
+        return root;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+
+            case R.id.button2:
+
+        }
     }
 }
