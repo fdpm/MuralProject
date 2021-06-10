46e2df2d27223b783d793460fc818854c0417891
Fabian Portilla <fdportillam1@gmail.com>
2021-06-10 01:14:20 -0500

    navigation view

---
+++ theMural/app/src/main/java/com/example/themural/NewItemFragment.java
@@ -0,0 +1,58 @@
+package com.example.themural;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link NewItemFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class NewItemFragment extends Fragment {
+
+
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    //private static final String ARG_PARAM1 = "param1";
+    //private static final String ARG_PARAM2 = "param2";
+
+
+    //private String mParam1;
+    //private String mParam2;
+
+    //
+
+    public NewItemFragment() {
+        // Required empty public constructor
+    }
+
+    public static NewItemFragment newInstance() {
+        NewItemFragment fragment = new NewItemFragment();
+        Bundle args = new Bundle();
+        //args.putString(ARG_PARAM1, param1);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    /*@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }*/
+
+
+    //El fragment se vuelve visible
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_new_item, container, false);
+    }
+}
