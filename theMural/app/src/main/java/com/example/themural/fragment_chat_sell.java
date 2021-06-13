9c8939a21c7f1c2d7cc8952651c362eafdc11551
TxusSmith <txus552@hotmail.com>
2021-06-12 23:20:55 -0500

    added fragment for chat

--- theMural/app/src/main/java/com/example/themural/fragment_chat_sell.java
+++ theMural/app/src/main/java/com/example/themural/fragment_chat_sell.java
@@ -1,19 +1,28 @@
 package com.example.themural;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.themural.adapter.ChatAdapter;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link fragment_chat_sell#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class fragment_chat_sell extends Fragment {
+public class fragment_chat_sell extends Fragment implements View.OnClickListener, ChatAdapter.OnChatListener {
+
+    private RecyclerView chatSellList;
+
+
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -41,6 +50,9 @@ public class fragment_chat_sell extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
@@ -51,6 +63,26 @@ public class fragment_chat_sell extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        View root = inflater.inflate(R.layout.fragment_chat_sell, container,false);
+        chatSellList = root.findViewById(R.id.chatSellList);
+        chatSellList.setHasFixedSize(true);
+        chatSellList.setOnClickListener(this);
+        //LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        //chatSellList.setLayoutManager(layoutManager);
+
+
         return inflater.inflate(R.layout.fragment_chat_sell, container, false);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+
+    @Override
+    public void onChatClick(int position) {
+        //Intent intent = new Intent(this, .class);
+        //intent.putExtra("selectedChat", );
+        //startActivity(intent);
     }
 }
