5ee82080b70c7b5e1c84f6f7f2360a634bba3e36
TxusSmith <txus552@hotmail.com>
2021-06-12 23:20:15 -0500

    edited message class in the model

--- theMural/app/src/main/java/com/example/themural/data/model/Message.java
+++ theMural/app/src/main/java/com/example/themural/data/model/Message.java
@@ -4,5 +4,15 @@ public class Message {
 
     private String content;
 
+    public Message(String content){
+        this.content=content;
+    }
 
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
 }
